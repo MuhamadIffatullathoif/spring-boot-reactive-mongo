1e042a52c4154b27b2d558b5c412a5cd8213d5c6
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-05 06:41:19 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springboot/reactivemongo/SpringBootReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springboot.reactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactiveMongoApplication.class, args);
+    }
+
+}

e98736ba9bdf9fc2a8035874f030479e41730e02
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-05 09:45:05 +0700

    06. Test Save new Document and Using awaitility not thread sleep

---
+++ src/test/java/com/iffat/springboot/reactivemongo/services/BeerServiceImplTest.java
@@ -0,0 +1,56 @@
+package com.iffat.springboot.reactivemongo.services;
+
+import com.iffat.springboot.reactivemongo.domain.Beer;
+import com.iffat.springboot.reactivemongo.mappers.BeerMapper;
+import com.iffat.springboot.reactivemongo.model.BeerDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerServiceImplTest {
+
+    @Autowired
+    BeerService beerService;
+
+    @Autowired
+    BeerMapper beerMapper;
+
+    BeerDTO beerDTO;
+
+    @BeforeEach
+    void setUp() {
+        beerDTO = beerMapper.beerToBeerDto(getTestBeer());
+    }
+
+    @Test
+    void testSaveBeer() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        Mono<BeerDTO> savedBeer = beerService.saveBeer(Mono.just(beerDTO));
+
+        savedBeer.subscribe(savedDto -> {
+            System.out.println(savedDto.getId());
+            atomicBoolean.set(true);
+        });
+
+        await().untilTrue(atomicBoolean);
+    }
+
+    public static Beer getTestBeer() {
+        return Beer.builder()
+                .beerName("Space Dust")
+                .beerStyle("IPA")
+                .price(BigDecimal.TEN)
+                .quantityOnHand(12)
+                .upc("231321")
+                .build();
+    }
+}

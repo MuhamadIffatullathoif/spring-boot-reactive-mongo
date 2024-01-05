3a675f7e4fcbcc4de492946afc7523aada293c76
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-05 10:57:28 +0700

    10. WebFlux Handler

---
+++ src/main/java/com/iffat/springboot/reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,19 @@
+package com.iffat.springboot.reactivemongo.web.fn;
+
+import com.iffat.springboot.reactivemongo.model.BeerDTO;
+import com.iffat.springboot.reactivemongo.services.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers() {
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}

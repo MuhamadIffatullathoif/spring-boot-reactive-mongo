9b5a3c6d3d90a42fb1976b5b2535b9660f762b4a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-07 09:13:20 +0700

    22. Spring Security Configuration

---
+++ src/main/java/com/iffat/springboot/reactivemongo/config/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.iffat.springboot.reactivemongo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(auth -> auth.anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+}
